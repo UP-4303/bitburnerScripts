7ecfed2e989b5b6c823f68fdb7fc032d6fcd187b
UP-4303 <up4303.pro@protonmail.com>
2023-09-17 00:05:44 +0200

    You can't hack your own servers. No need to try.

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -7,12 +7,30 @@ export async function main(ns) {
 
 /** @param {NS} ns */
 export async function watcher(ns, targets, runOnTrigger) {
+	ns.disableLog("ALL");
 	let scriptLevel;
 	let markedForRemoval = [];
 	if (!ns.fileExists("/signals/newHackedServers.txt")){
 		await ns.prompt("WARNING : '/signals/newHackedServers.txt' does not exists !");
 		return
 	}
+
+	// Remove personal servers
+	for(let targetIndex in targets){
+		if(targets[targetIndex].name == 'home' || targets[targetIndex].name.match(/server-[0-9]*/)){
+			markedForRemoval.push(targetIndex);
+		}
+	}
+
+	markedForRemoval.sort((a, b) => b - a);
+	
+	// Delete the marked for removal
+	while (markedForRemoval.length > 0) {
+		targets.splice(markedForRemoval[0], 1);
+		markedForRemoval.shift();
+	}
+
+
 
 	// Try until everything is hacked
 	while (targets.length > 0) {
@@ -23,7 +41,6 @@ export async function watcher(ns, targets, runOnTrigger) {
 			let enoughPorts = ns.getServerNumPortsRequired(targets[targetIndex].name) <= scriptLevel;
 			let enoughLevels = ns.getServerRequiredHackingLevel(targets[targetIndex].name) <= ns.getHackingLevel();
 			if (enoughPorts && enoughLevels) {
-				ns.print("WATCHER 26 ", targets[targetIndex]);
 				await runOnTrigger(ns, targets[targetIndex]);
 				markedForRemoval.push(targetIndex);
 				ns.write("/signals/newHackedServers.txt", ','+targets[targetIndex].name, 'a');
@@ -37,6 +54,11 @@ export async function watcher(ns, targets, runOnTrigger) {
 			targets.splice(markedForRemoval[0], 1);
 			markedForRemoval.shift();
 		}
+		
+		ns.enableLog('print');
+		ns.print("==========");
+		ns.print("REMAINING TARGETS : ", targets);
+		ns.disableLog('ALL');
 
 		await ns.sleep(10000);
 	}
